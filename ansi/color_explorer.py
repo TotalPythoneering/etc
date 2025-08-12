82e9bd77f562663b1c9d0f0fdde5df920bc31281
Mr. Nagy <randall.nagy@gmail.com>
2025-08-12 06:40:22 -0400

    Video Code + Link
    From forcing us to junk our non-broke computers to so many classic colorized console 'apps freedom, portability as well as hugely higher-performance options have ever 'been there' ... if 'ya know where to look!

--- ansi/color_explorer.py
+++ ansi/color_explorer.py
@@ -1,4 +1,15 @@
-for s, x in enumerate(range(1,100),1):
-    print(f' \33[{x}m{x:02}\33[0m', end='')
+# We're using W.S.L, a.k.a
+# "Windows Subsystem for 
+# Linux" - Debian.
+# Video: https://ko-fi.com/post/Colorizing-Your-App--T-U-I-Style-C0C31JKS47
+
+ESC = '\33[' # Escape 
+RESET = ESC + '0m' # Yup
+
+r = range(1,100)
+for s,x in enumerate(r,1):
+    print(f'{ESC}{x}m',
+          f'{x:02}{RESET}',
+          end='')
     if s % 10 == 0: print()
 print()
